f0bd30c6432b2cd1bea2a95f685359b06426aa6d
Sakshi Mawar <89383033+sakshimawar117@users.noreply.github.com>
2022-01-31 17:43:46 +0530

    removed

--- src/layout/util/userUtil.js
+++
@@ -1,8 +0,0 @@
-export const isLoggedIn = () => {
-    return !!localStorage.getItem(process.env.REACT_APP_LOGIN_TOKEN);
-};
-
-export const getToken = () => {
-    return localStorage.getItem(process.env.REACT_APP_LOGIN_TOKEN);
-};
-
